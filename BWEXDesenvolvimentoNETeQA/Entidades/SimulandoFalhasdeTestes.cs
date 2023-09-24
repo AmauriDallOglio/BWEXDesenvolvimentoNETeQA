92205245bab55b015585fe4b80ed3c5fea015609
Amauri Dall'Oglio <amauridalloglio@outlook.com.br>
2023-09-24 10:32:04 -0300

    Adicionar arquivos de projeto.

---
+++ BWEXDesenvolvimentoNETeQA/Entidades/SimulandoFalhasdeTestes.cs
@@ -0,0 +1,30 @@
+﻿namespace BWEXDesenvolvimentoNETeQA.Entidades
+{
+    public class SimulandoFalhasdeTestes
+    {
+        public void Executar()
+        {
+            Console.Write("Digite o nome do teste: ");
+            string nomeDoTeste = Console.ReadLine();
+
+            // Solicite ao usuário que digite a descrição do erro:
+            Console.Write("Digite a descrição do erro: ");
+            string descricaoDoErro = Console.ReadLine();
+
+            // Use switch case para determinar a saída com base no nome do teste:
+            switch (nomeDoTeste)
+            {
+                case "Teste de Compra Online O carrinho de compras nao atualiza":
+                    Console.WriteLine($"O teste falhou: Descrição do erro: {descricaoDoErro}");
+                    break;
+                case "Teste de API Rest A solicitacao GET retornou erro 404":
+                    Console.WriteLine($"O teste falhou: Descrição do erro: {descricaoDoErro}");
+                    break;
+                case "Teste de Envio de Formulario O formulario nao foi enviado":
+                    Console.WriteLine($"O teste falhou: Descrição do erro: {descricaoDoErro}");
+                    break;
+            }
+        }
+
+    }
+}
---
+++ BWEXDesenvolvimentoNETeQA/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using BWEXDesenvolvimentoNETeQA.Entidades;
+
+SimulandoFalhasdeTestes exercio1 = new SimulandoFalhasdeTestes();
+exercio1.Executar();
+
+
+
+
+
+
+
+Console.WriteLine("Hello, World!");
