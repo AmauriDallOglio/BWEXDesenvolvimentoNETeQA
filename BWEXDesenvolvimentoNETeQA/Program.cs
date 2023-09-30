4e0f6902268815f99ff446ec1fce23d1fff4b1ba
Amauri Dall'Oglio <amauridalloglio@outlook.com.br>
2023-09-30 14:01:49 -0300

    desafio

--- BWEXDesenvolvimentoNETeQA/Program.cs
+++ BWEXDesenvolvimentoNETeQA/Program.cs
@@ -3,12 +3,17 @@
 
 using BWEXDesenvolvimentoNETeQA.Entidades;
 
-SimulandoFalhasdeTestes exercio1 = new SimulandoFalhasdeTestes();
-exercio1.Executar();
+//SimulandoFalhasdeTestes exercio1 = new SimulandoFalhasdeTestes();
+//exercio1.Executar();
 
 
+//TiposOperadoresAritmeticos exe2 = new TiposOperadoresAritmeticos();
+//exe2.Executar();
 
+//EstruturasCondicionais exe3 = new EstruturasCondicionais();
+//exe3.Executar();
 
-
+ArraysListas exe4 = new ArraysListas();
+exe4.Executar();
 
 Console.WriteLine("Hello, World!");
